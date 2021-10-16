0d6204dc3658abbd67ddadd78ad3838af0341109
Jessica Watson <jessica.watson1@live.com>
2021-10-16 16:20:19 -0600

    Updated Js

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -2,12 +2,12 @@ const barIcon = document.querySelector(".navbars")
 const closeIcon = document.querySelector(".navclose")
 const navMenu = document.querySelector(".sidebar")
 
-
-
 const slideOut = function () {
+  //toggle function for the nav menu
   navMenu.classList.toggle("change");
   barIcon.classList.toggle("openbars")
   closeIcon.classList.toggle("closebars")
 }
+//click listeners for open and close icon
 barIcon.addEventListener('click', slideOut)
 closeIcon.addEventListener('click', slideOut)
